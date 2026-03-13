b8bed762d824a18fc63ab35bfe25277e3656297d
Robeel Bhatti <robeelbhatti19@gmail.com>
2026-03-12 20:25:59 -0400

    fix: add IAM service to make boto3 API calls.

---
+++ src/iam_service.py
@@ -0,0 +1,28 @@
+from mypy_boto3_iam import IAMClient
+
+class IAMService:
+
+    def __init__(self, client: IAMClient) -> None:
+        self.client = client
+
+    def create_role(self, role_name: str, policy_doc: str, description: str):
+        self.client.create_role(
+            RoleName=role_name,
+            AssumeRolePolicyDocument=policy_doc,
+            Description=description
+        )
+
+    def attach_managed_policies(self, role_name: str, policy: str):
+        self.client.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn=policy,
+        )
+        
+    def attach_inline_policies(self, role_name: str, policy_name: str, policy: str):
+        self.client.put_role_policy(
+            RoleName=role_name,
+            PolicyName=policy_name,
+            PolicyDocument=policy
+        )
+
+
